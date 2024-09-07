4d6760c2ea0e8a37a1139b89b9f1d27051baff07
pedroonovais <phenrique101007@gmail.com>
2024-09-07 14:05:38 -0300

    Cria estilo global

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components"
+
+export const GlobalStyles = createGlobalStyle`
+    :root{
+        --primary-color: #0774f7;
+    }
+
+    *{
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+    }
+
+    body{
+        font-size: 16px;
+    }
+`
