6c436fcdb61f901aec66948db71ceda52efacd23
pedroonovais <phenrique101007@gmail.com>
2024-09-08 13:23:50 -0300

    Cria pagina nova-lista

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const LazyHome = lazy(() => import("./pages/Home/Home"))
 const LazyProfile = lazy(() => import("./pages/Profile/Profile"))
+const LazyNewList = lazy(() => import("./pages/NewList/NewList"))
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
                 <Routes>
                     <Route path="/" element={<LazyHome/>}/>
                     <Route path="/perfil" element={<LazyProfile/>}/>
+                    <Route path="/nova-lista" element={<LazyNewList/>}/>
                 </Routes>
             </Suspense>
         </BrowserRouter>
---
+++ src/pages/NewList/NewList.tsx
@@ -0,0 +1,10 @@
+import { Layout } from "../../components/Layout/Layout";
+import { Title } from "../../components/Title/Title";
+
+export default function NewList(){
+    return(
+        <Layout>
+            <Title>Nova lista</Title>
+        </Layout>
+    )
+}
